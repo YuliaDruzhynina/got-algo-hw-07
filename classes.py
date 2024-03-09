49ac23568ac71b1dd19aacae1515d49db8950c47
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-03-09 13:55:34 +0200

    Update classes.py
    change show_birthday

--- classes.py
+++ classes.py
@@ -7,24 +7,25 @@ class Field:
         self.value = value
 
     def is_valid(self, value):
+        self.value = value
         return True
     
     @property
     def value(self):
-        return self.__value
+        return self.__value  # можем указать иное значение для возврата напр.множина
     
     @value.setter
     def value(self, value):
         if not self.is_valid(value):
             raise ValueError
-        else:
-            self.__value = value
+        self.__value = value
     
     def __str__(self) -> str:
         return str(self.value)
 
     
 class Birthday(Field):
+
     def is_valid(self, value):
         if not datetime.strptime(value, "%d.%m.%Y"):
             return False
@@ -38,11 +39,10 @@ class Birthday(Field):
     def value(self, value):
         if not self.is_valid(value):
             raise ValueError
-        else:
-            self.__value = datetime.strptime(value, "%d.%m.%Y")
+        self.__value = datetime.strptime(value, "%d.%m.%Y")
+            
 
-
-class Name(Field):  
+class Name(Field):
     def is_valid(self, value):
         return bool(value)
 
@@ -59,9 +59,9 @@ class Record:
         self.birthday = None
 
     def add_birthday(self, birthday: str):
-        self.birthday = Birthday(birthday)        
-
-    def add_phone(self, phone):  #self.phones.append(Phone(phone))
+        self.birthday = Birthday(birthday)     
+        
+    def add_phone(self, phone):  # self.phones.append(Phone(phone))
         ph = Phone(phone)
         self.phones.append(ph)
         return ph
@@ -72,19 +72,20 @@ class Record:
                 return ph 
         return None
     
-    def remove_phone(self, phone):       
+    def remove_phone(self, phone):    
         try:
             self.phones.remove(self.find_phone(phone))
         except ValueError:
-            return None  
-             
+            return "Not found"
+  
+           
     def edit_phone(self, old_phone: str, new_phone: str):
-        ph = self.find_phone(old_phone)
-        if ph:
+        if self.find_phone(old_phone):
             self.remove_phone(old_phone)
             self.add_phone(new_phone)
         else:
             raise ValueError
+        
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, birthday: {str(self.birthday)}"
@@ -92,51 +93,23 @@ class Record:
 
 class AddressBook(UserDict):
     def add_record(self, record: Record):
-        self.data[record.name.value] = record
+        self.data[record.name.value] = record  # self[record.name.value]
         return record
     
-    def find(self, name: str):                
-        return self.data.get(name)
+    def find(self, name: str):          
+        return self.data.get(name)  # return self.get(name) error не будет:UserDict
     
     def delete(self, name: str):
         if self.find(name):
             del self.data[name]
 
     def get_upcoming_birthdays(self):
+        
         today = datetime.now().date()
         upcoming_birthdays = [record for record in self.data.values() if record.birthday.value.date() > today]
+        upcoming_birthdays += upcoming_birthdays  #upcoming_birthday.append(record)
         return upcoming_birthdays
 
     def __str__(self):
         return "\n".join(str(record) for record in self.data.values())     
 
-    # Створення нової адресної книги
-# book = AddressBook()
-
-#     # Створення запису для John
-# john_record = Record("John")
-# john_record.add_phone("1234567890")
-# john_record.add_phone("5555555555")
-#     # Додавання запису John до адресної книги
-# book.add_record(john_record)
-
-#     # Створення та додавання нового запису для Jane
-# jane_record = Record("Jane")
-# jane_record.add_phone("9876543210")
-# book.add_record(jane_record)
-
-#     # Виведення всіх записів у книзі
-# for name, record in book.data.items():
-#     print(record)
-#     # Знаходження та редагування телефону для John
-# john = book.find("John")
-# john.edit_phone("1234567890", "1112223333")
-
-# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-#     # Пошук конкретного телефону у записі John
-# found_phone = john.find_phone("5555555555")
-# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-
-#     # Видалення запису Jane
-# book.delete("Jane")
